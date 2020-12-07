4ced94c4ca71e24f09751858f28a5540dfbb3aaf
limiaochen <notmot@163.com>
2020-12-07 22:38:40 +0800

    yaml file read util added by Michael

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
-import ndpdns "github.com/xavier-niu/npd-dns/pkg/ndp_dns"
+import (
+	ndpdns "github.com/xavier-niu/npd-dns/pkg/ndp_dns"
+	"gopkg.in/yaml.v2"
+	"log"
+)
 
-func main()  {
-	var googleRecords = map[string]string {
-		"mail.google.com": "192.168.0.2",
+var yamlData = ""
+
+func main() {
+	var googleRecords = map[string]string{
+		"mail.google.com":  "192.168.0.2",
 		"paste.google.com": "192.168.0.3",
+	}
+	firstMap := make(map[string]map[string]string)
+	err := yaml.Unmarshal([]byte(yamlData), &firstMap)
+	if err != nil {
+		log.Fatalf("error: %v", err)
 	}
 	dns := ndpdns.NewServer(10001)
 	dns.AddZoneData("google.com", googleRecords, nil, ndpdns.DNSForwardLookupZone)
